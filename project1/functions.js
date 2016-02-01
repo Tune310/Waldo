2482cdd995e34a829076dae7aec8d5e3fbd01ee4
Tune <Reidcrawford55@gmail.com>
2016-01-31 16:44:12 -0800

    Linked jQuery Trying to make a click event happen with the arrayColors I have set up

--- project1/functions.js
+++ project1/functions.js
@@ -1,9 +1,22 @@
 document.ready(function(){
+alert("working");
 
 var playerOne
 var playerTwo
 var points
-var arrayColors = {'red,' 'green', 'blue', 'yellow', 'orange',}
+var arrayColors = {'.red,' '.green', '.blue', '.yellow', '.orange',}
+
+var colorSwitch = function() {
+	if(i = 0; i < arrayColors.length; i += 1)
+}
+$('div#one').click(function(){
+
+	 $("div").addClass("blue")
+
+	})
+
 
 })
+
+
 
