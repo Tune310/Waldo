c4a84d48e1c26295b413fe8982d802bc91b77b16
Tune <Reidcrawford55@gmail.com>
2016-02-01 11:37:50 -0800

    made changes, changed classes colOne colTwo and colThree and made them all into class colors. Changed the CSS targeting the sections with the divs and added the class color

--- project1/functions.js
+++ project1/functions.js
@@ -4,20 +4,16 @@ alert("working");
 var playerOne
 var playerTwo
 var points
-var arrayColors = {'red,' 'green', 'blue', 'yellow', 'orange',}
+var Colors = {'red,' 'green', 'blue', 'yellow', 'orange',}
 var red =("background-color", "red");
 var green = ("background-color", "green");
 var blue = ("background-color", "blue");
 var yellow = ("background-color", "yellow");
 var orange = ("background-color", "orange");
 
-	for(var i = 0; i < arrayColors.length; i += 1){
+	for(var i = 0; i < Colors.length; i += 1)
 		
-		function changeColor() {
 
-		}
-	}
-})
 
 ///Look at this than delete it
 $("p").on({
@@ -32,3 +28,5 @@ $("p").on({
     } 
 }); /// Conitinue here by working with this
 
+
+// Phillipe color change
