4d4a3a48e252f9958202e272d1caac5d4c3937cd
Tune <Reidcrawford55@gmail.com>
2016-02-02 16:11:06 -0800

    divs eacn generate a random background image

--- project1/js/functions.js
+++ project1/js/functions.js
@@ -1,4 +1,4 @@
-$('document').ready(function(){
+// $('document').ready(function(){
 
 alert('working')
 
@@ -13,15 +13,24 @@ var colors = ['red', 'green', 'blue', 'yellow', 'orange']
 // var orange = ("background-color", "orange");
 var colorBox = $('.color')
 var pics = ['santa', 'hankey', 'cartman', 'waldo', 'santa', 'decoyWaldo']
+var divs = ['div#one', 'div#two', 'div#three', 'div#four', 'div#five', 'div#six', 'div#seven', 'div#eight', 'div#nine']
 
 
 
-		colorBox.click(function(){
-			$(this).addClass(colors[Math.floor(Math.random() * colors.length)])
-			// alert('works')
-		})
-	
-
-});
+		// colorBox.click(function(){
+		// 	$(this).addClass(colors[Math.floor(Math.random() * colors.length)])
+		// 	// alert('works')
+		// })
 		
+		$('#play').click(function() {
+			var random = pics[Math.floor(Math.random()*pics.length)]
+			var whatDiv = divs[Math.floor(Math.random()*divs.length)]
 
+			$('.color').each(function(){
+				$(this).css('background-image', 'url("css/images/' + pics[Math.floor(Math.random()*pics.length)] + '.jpg")')
+			})
+			console.log('working')
+		})
+		//next get pics into the divs after play button is clicked
+// });
+		
