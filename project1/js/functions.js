afcbb8b0d45822437b1faa796b783268dc8bbac2
Tune <Reidcrawford55@gmail.com>
2016-02-03 14:52:41 -0800

    Finally got the play button to work properly, With the one and only Waldo

--- project1/js/functions.js
+++ project1/js/functions.js
@@ -1,7 +1,3 @@
-// $('document').ready(function(){
-
-alert('working')
-
 var playerOne
 var playerTwo
 var points
@@ -11,9 +7,31 @@ var colors = ['red', 'green', 'blue', 'yellow', 'orange']
 // var blue = ("background-color", "blue");
 // var yellow = ("background-color", "yellow");
 // var orange = ("background-color", "orange");
-var colorBox = $('.color')
-var pics = ['santa', 'hankey', 'cartman', 'waldo', 'santa', 'decoyWaldo']
-var divs = ['div#one', 'div#two', 'div#three', 'div#four', 'div#five', 'div#six', 'div#seven', 'div#eight', 'div#nine']
+var santa = '<img class="santa" src="../css/images/santa.jpg"/>'
+var hankey = '<img class="hankey" src="css/images/hankey.jpg"/>'
+var cartman = '<img class="cartman" src="css/images/cartman.jpg"/>'
+var decoyWaldo = '<img class="decoyWaldo" src="css/images/decoyWaldo.jpg"/>'
+var waldo = '<img class="waldo" src="css/images/waldo.jpg"/>'
+var katyPerry = '<img class="katyPerry" src="css/images/katyPerry.jpg"/>'
+var jesusSanta = '<img class="jesusSanta" src="css/images/jesusSanta.jpg"/>'
+var theGrinch = '<img class="theGrinch" src="css/images/theGrinch.jpg"/>'
+var jenniferLawrence = '<img class="jenniferLawrence" src="css/images/jenniferLawrence.jpg"/>'
+var boxes = $('.box')
+var pics = [santa, hankey, cartman, decoyWaldo, waldo, katyPerry, jesusSanta, theGrinch, jenniferLawrence]
+var div1 = document.getElementById('div#one');
+var div2 = document.getElementById('div#two');
+var div3 = document.getElementById('div#three');
+var div4 = document.getElementById('div#four');
+var div5 = document.getElementById('div#five');
+var div6 = document.getElementById('div#six');
+var div7 = document.getElementById('div#seven');
+var div8 = document.getElementById('div#eight');
+var div9 = document.getElementById('div#nine'); 
+var randomDiv = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+var waldo = "div" + randomDiv;
+var divs = ['div1',  'div2',  'div3',  'div4',  'div5', 'div6', 'div7', 'div8', 'div9']
+
+
 
 
 
@@ -23,14 +41,49 @@ var divs = ['div#one', 'div#two', 'div#three', 'div#four', 'div#five', 'div#six'
 		// })
 		
 		$('#play').click(function() {
-			var random = pics[Math.floor(Math.random()*pics.length)]
-			var whatDiv = divs[Math.floor(Math.random()*divs.length)]
+			// var random = pics[Math.floor(Math.random()*pics.length)]
+			// var whatDiv = divs[Math.floor(Math.random()*divs.length)]
 
-			$('.color').each(function(){
-				$(this).css('background-image', 'url("css/images/' + pics[Math.floor(Math.random()*pics.length)] + '.jpg")')
+			function fisherYates ( myArray ) {
+  			var i = myArray.length;
+  			if ( i == 0 ) return false;
+  			while ( --i ) {
+     			var j = Math.floor( Math.random() * ( i + 1 ) );
+     			var tempi = myArray[i];
+     			var tempj = myArray[j];
+     			myArray[i] = tempj;
+     			myArray[j] = tempi;
+  			 	}
+			}
+
+			for(var i = 0; i < boxes.length; i++) {
+				fisherYates(pics)
+				boxes[i].innerHTML = pics.splice(0,1);
+			}
+
+			// $('.color').each(function(){
+			// 	$(this).css('background-image', 'url("css/images/' + pics[Math.floor(Math.random()*pics.length)] + '.jpg")')
+			
+		// 	function wheresWaldo() {
+		// 	var divs = ['div1', 'div2', 'div3', 'div4', 'div5', 'div6', 'div7', 'div8', 'div9']
+		// 	var randomDiv = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+		// 	var waldo = divs + randomDiv;
+		// 	$(waldo).css('background-image', "url(css/images/waldo.jpg)")
+		// 	console.log('works');
+		// }
+
 			})
-			console.log('working')
-		})
+			// wheresWaldo();
+		// })
+
+		// function wheresWaldo() {
+		// 	var divs = ['div1', 'div2', 'div3', 'div4', 'div5', 'div6', 'div7', 'div8', 'div9']
+		// 	var randomDiv = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+		// 	var waldo = divs + randomDiv;
+		// 	$(waldo).css('background-image', "url(css/images/waldo.jpg)")
+		// 	console.log('works');
+		// }
+
 		//next get pics into the divs after play button is clicked
 // });
 		
