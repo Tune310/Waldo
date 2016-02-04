b59d31860e2824becfba24954ae390ba07f96942
Tune <Reidcrawford55@gmail.com>
2016-02-03 16:46:26 -0800

    waldo +1 Yeeeeeeeeeeeeeeeeee

--- project1/js/functions.js
+++ project1/js/functions.js
@@ -7,7 +7,7 @@ var colors = ['red', 'green', 'blue', 'yellow', 'orange']
 // var blue = ("background-color", "blue");
 // var yellow = ("background-color", "yellow");
 // var orange = ("background-color", "orange");
-var santa = '<img class="santa" src="../css/images/santa.jpg"/>'
+var santa = '<img class="santa" src="css/images/santa.jpg"/>'
 var hankey = '<img class="hankey" src="css/images/hankey.jpg"/>'
 var cartman = '<img class="cartman" src="css/images/cartman.jpg"/>'
 var decoyWaldo = '<img class="decoyWaldo" src="css/images/decoyWaldo.jpg"/>'
@@ -16,21 +16,38 @@ var katyPerry = '<img class="katyPerry" src="css/images/katyPerry.jpg"/>'
 var jesusSanta = '<img class="jesusSanta" src="css/images/jesusSanta.jpg"/>'
 var theGrinch = '<img class="theGrinch" src="css/images/theGrinch.jpg"/>'
 var jenniferLawrence = '<img class="jenniferLawrence" src="css/images/jenniferLawrence.jpg"/>'
-var boxes = $('.box')
-var pics = [santa, hankey, cartman, decoyWaldo, waldo, katyPerry, jesusSanta, theGrinch, jenniferLawrence]
-var div1 = document.getElementById('div#one');
-var div2 = document.getElementById('div#two');
-var div3 = document.getElementById('div#three');
-var div4 = document.getElementById('div#four');
-var div5 = document.getElementById('div#five');
-var div6 = document.getElementById('div#six');
-var div7 = document.getElementById('div#seven');
-var div8 = document.getElementById('div#eight');
-var div9 = document.getElementById('div#nine'); 
-var randomDiv = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
-var waldo = "div" + randomDiv;
-var divs = ['div1',  'div2',  'div3',  'div4',  'div5', 'div6', 'div7', 'div8', 'div9']
 
+var boxes = $('.box')
+
+var pics = [santa, hankey, cartman, decoyWaldo, waldo, katyPerry, jesusSanta, theGrinch, jenniferLawrence]
+
+// var randomDiv = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+// var waldo = "div" + randomDiv;
+// var divs = ['div1',  'div2',  'div3',  'div4',  'div5', 'div6', 'div7', 'div8', 'div9']
+
+function shufflePics() {
+			// var random = pics[Math.floor(Math.random()*pics.length)]
+			// var whatDiv = divs[Math.floor(Math.random()*divs.length)]
+
+			function shuffleArray ( myArray ) {
+	  			var i = myArray.length;
+	  			if ( i == 0 ) return false;
+	  			while ( --i ) {
+	     			var j = Math.floor( Math.random() * ( i + 1 ) );
+	     			var tempi = myArray[i];
+	     			var tempj = myArray[j];
+	     			myArray[i] = tempj;
+	     			myArray[j] = tempi;
+	  			 	}
+			}
+			
+			shuffleArray(pics)
+			
+			for(var i = 0; i < boxes.length; i++) {
+
+				boxes[i].innerHTML = pics[i];
+			}
+	}
 
 
 
@@ -40,26 +57,34 @@ var divs = ['div1',  'div2',  'div3',  'div4',  'div5', 'div6', 'div7', 'div8', 
 		// 	// alert('works')
 		// })
 		
-		$('#play').click(function() {
-			// var random = pics[Math.floor(Math.random()*pics.length)]
-			// var whatDiv = divs[Math.floor(Math.random()*divs.length)]
+		var startGame = function() {
+			window.setInterval(shufflePics, 750)
+		}
 
-			function fisherYates ( myArray ) {
-  			var i = myArray.length;
-  			if ( i == 0 ) return false;
-  			while ( --i ) {
-     			var j = Math.floor( Math.random() * ( i + 1 ) );
-     			var tempi = myArray[i];
-     			var tempj = myArray[j];
-     			myArray[i] = tempj;
-     			myArray[j] = tempi;
-  			 	}
-			}
+		$('button').click(startGame);
 
-			for(var i = 0; i < boxes.length; i++) {
-				fisherYates(pics)
-				boxes[i].innerHTML = pics.splice(0,1);
-			}
+		$('body').on('click', '.waldo', function(){
+ 			console.log('+1')
+ 		
+ 		})
+		// $(waldo).click(function(){
+		// 	$(this).data('clicked', true)
+		// });
+
+		// if($(waldo).data('clicked')) {
+		// 	i = 0
+		// 	points = i++
+		// 	console.log(points)
+			
+
+		// }
+
+		
+		// if(waldo === true) {
+		// 	points = += i + 1
+		// }else{
+		// 	alert('You Suck!!')
+		// }
 
 			// $('.color').each(function(){
 			// 	$(this).css('background-image', 'url("css/images/' + pics[Math.floor(Math.random()*pics.length)] + '.jpg")')
@@ -72,8 +97,7 @@ var divs = ['div1',  'div2',  'div3',  'div4',  'div5', 'div6', 'div7', 'div8', 
 		// 	console.log('works');
 		// }
 
-			})
-			// wheresWaldo();
+						// wheresWaldo();
 		// })
 
 		// function wheresWaldo() {
